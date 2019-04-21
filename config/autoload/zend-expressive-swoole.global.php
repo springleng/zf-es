46eb928097a3905a9771c5aacac7dcf505b22d32
silen <361494897@qq.com>
2019-04-21 10:34:42 +0800

    swoole config

---
+++ config/autoload/zend-expressive-swoole.global.php
@@ -0,0 +1,19 @@
+<?php
+
+use Zend\Expressive\Swoole\ConfigProvider;
+
+return array_merge((new ConfigProvider())(), [
+    'zend-expressive-swoole' => [
+        'enable_coroutine' => true,
+        'swoole-http-server' => [
+            'host' => '127.0.0.1',
+            'port' => 8088,
+            'mode' => SWOOLE_PROCESS, // SWOOLE_BASE,
+            'options' => [
+                'log_level' => SWOOLE_LOG_INFO,
+                'worker_num' => swoole_cpu_num() * 2,
+                'enable_coroutine' => true,
+            ],
+        ],
+    ],
+]);
